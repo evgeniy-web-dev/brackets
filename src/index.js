be4d3f36b21182b70e73064df061f6695a5a07f1
evgeniy-web-dev <vem-89@mail.ru>
2021-03-10 03:35:45 +0300

    feat: implements brackets check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
+// Скобки
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let sameOpenCloseBrackets = '';
+    let brackets = '';
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let config = bracketsConfig[i];
+
+        let openBracket = config[0];
+        let closeBracket = config[1];
+        if (closeBracket === openBracket) {
+            sameOpenCloseBrackets += openBracket;
+        }
+
+        brackets += openBracket;
+        brackets += closeBracket;
+    }
+
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+        let bracket = str[i];
+        let index = brackets.indexOf(bracket);
+        if (sameOpenCloseBrackets.includes(bracket)) {
+            let lastExpBraketIndex = stack[stack.length - 1];
+            if (lastExpBraketIndex === index) {
+                stack.pop();
+                continue;
+            } else {
+                stack.push(index);
+                continue;
+            }
+        }
+
+        if (index % 2 === 0) {
+            stack.push(index + 1);
+        } else {
+            let expIndex = stack.pop();
+            if (index !== expIndex) {
+                return false;
+            }
+        }
+    }
+    if (stack.length > 0) {
+        return false;
+    }
+
+    return true;
+};
